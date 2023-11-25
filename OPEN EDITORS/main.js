4d0d544aa0eb0ff09dd9cc8329beaf6945551676
HeatherSmallwood <heathersmallwood.hs@gmail.com>
2023-11-25 16:31:26 -0500

    weather homework

---
+++ OPEN EDITORS/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const API_KEY = 'e4dd4e3c19c37c38ca18e9b66fb051fe'; 
+    const ZIP_CODE = '33460'; 
+    const UNITS = 'imperial'; 
+
+    const weatherInfo = {
+        highTemp: document.getElementById('highTemp'),
+        lowTemp: document.getElementById('lowTemp'),
+        forecast: document.getElementById('forecast'),
+        humidity: document.getElementById('humidity')
+    };
+
+    const fetchWeatherData = async () => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${ZIP_CODE}&units=${UNITS}&appid=${API_KEY}`);
+            if (!response.ok) {
+                throw new Error('Weather data not available');
+            }
+            const data = await response.json();
+
+            weatherInfo.highTemp.innerText = data.main.temp_max.toFixed(1);
+            weatherInfo.lowTemp.innerText = data.main.temp_min.toFixed(1);
+            weatherInfo.forecast.innerText= data.weather[0].description;
+            weatherInfo.humidity.innerText = data.main.humidity;
+        } catch (error) {
+            console.error('Error:', error.message);
+            alert('Failed to fetch weather data');
+        }
+    };
+
+    fetchWeatherData();
+});
