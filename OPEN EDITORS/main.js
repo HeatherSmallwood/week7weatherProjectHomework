cbdb9be40fd8bc0b68302c73f4e66aea067f9f2d
HeatherSmallwood <heathersmallwood.hs@gmail.com>
2023-11-27 19:39:52 -0500

    final

--- OPEN EDITORS/main.js
+++ OPEN EDITORS/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_KEY = 'e4dd4e3c19c37c38ca18e9b66fb051fe'; 
-    const ZIP_CODE = '33460'; 
-    const UNITS = 'imperial'; 
+    const API_KEY = 'e4dd4e3c19c37c38ca18e9b66fb051fe';
+    let ZIP_CODE = ''; 
+    const UNITS = 'imperial';
 
     const weatherInfo = {
         highTemp: document.getElementById('highTemp'),
@@ -9,6 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
         forecast: document.getElementById('forecast'),
         humidity: document.getElementById('humidity')
     };
+
+    const zipCodeInput = document.getElementById('zipCode');
+    const submitButton = document.getElementById('submitZip');
 
     const fetchWeatherData = async () => {
         try {
@@ -20,15 +23,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
             weatherInfo.highTemp.innerText = data.main.temp_max.toFixed(1);
             weatherInfo.lowTemp.innerText = data.main.temp_min.toFixed(1);
-            weatherInfo.forecast.innerText= data.weather[0].description;
+            weatherInfo.forecast.innerText = data.weather[0].description;
             weatherInfo.humidity.innerText = data.main.humidity;
+
+            
+            document.querySelector('.weather-info').style.display = 'block';
         } catch (error) {
             console.error('Error:', error.message);
             alert('Could not get the weather data!');
         }
     };
 
-    fetchWeatherData();
+    submitButton.addEventListener('click', () => {
+        ZIP_CODE = zipCodeInput.value.trim();
+        if (ZIP_CODE) {
+            fetchWeatherData();
+        } else {
+            alert('Please enter a valid ZIP code.');
+        }
+    });
 });
 
 
